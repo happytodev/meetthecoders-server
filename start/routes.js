65017bab4e5cae3adfc6485bfa1b444ce447bc13
Frédéric Blanc <frederic.blanc@itanea.fr>
2019-10-04 08:48:27 +0200

    Meet The Coders Adonis Server v0.1

--- start/routes.js
+++ start/routes.js
@@ -17,3 +17,9 @@
 const Route = use('Route')
 
 Route.on('/').render('welcome')
+
+Route.group(() => {
+  Route.post('login', 'Auth/AuthenticationController.login')
+  Route.post('register', 'Auth/AuthenticationController.register')
+  Route.get('me', 'Auth/AuthenticationController.me').middleware(['auth'])
+}).prefix('api')
